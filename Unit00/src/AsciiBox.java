b68b1c1282a75c2b03bb1b85e7c72d24d361b123
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-15 11:25:35 -0800

    Box Update

--- Unit00/src/AsciiBox.java
+++ Unit00/src/AsciiBox.java
@@ -10,7 +10,7 @@ public class AsciiBox
 {
 	public static void main(String[] args)
 	{
-		System.out.println("name \t  date \n\n" );
+		System.out.println("Emma Cao \t  1/15/22 \n\n" );
 		System.out.println("+++++++++++++++++++++++++");
 		System.out.println("-------------------------");
 		System.out.println("ooooooooooooooooooooooooo");
