ac0dc1886a254d9ca097df04cd268a250b7267ee
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-15 16:30:08 -0800

    Variable Update

--- Unit00/src/Variables.java
+++ Unit00/src/Variables.java
@@ -1,9 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Emma Cao
+//Date - 1/15/22
+//Class - APCSA
 //Lab  -
 
 public class Variables
@@ -18,15 +18,20 @@ public class Variables
 
 		//integer variables
 		byte byteOne = 127;
-
+		short shortOne = 19;
+		int intOne = 176785;
+		long longOne = 123254369;
 
 		//decimal variables
-
+		float floatOne = 10;
+		double doubleOne = 293.11;
 
 		//other integer types
-
+		char charOne = 'a';
 
 		//other types
+		boolean booleanOne = true;
+		String stringOne = "hello!";
 
 
 		//output your information here
@@ -35,6 +40,31 @@ public class Variables
 		System.out.println("*        integer types          *");
 		System.out.println("*                               *");
 		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*32 bit - intOne = "+intOne+"\t*");
+		System.out.println("*64 bit - longOne = "+longOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*         real types            *");
+		System.out.println("*                               *");
+		System.out.println("*32 fit - floatOne = "+floatOne+"\t*");
+		System.out.println("*64 bit - doubleOne = "+doubleOne+"\t*");
+		System.out.println("*                               *");
+		System.out.println("*     other integer types       *");
+		System.out.println("*                               *");
+		System.out.println("*16 bit - charOne = "+charOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*         other types           *");
+		System.out.println("*                               *");
+		System.out.println("*booleanOne = "+booleanOne+"\t\t*");
+		System.out.println("*stringOne = "+stringOne+"\t\t*");
+		System.out.println("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\");
+		
+		
+		
+		
+		
+		
+		
 
 
 
