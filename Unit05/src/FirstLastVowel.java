a1448c14b4e67b8170709f593ddf194cdcb02c08
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-24 09:29:39 -0800

    FirstLastVowel Update

--- Unit05/src/FirstLastVowel.java
+++ Unit05/src/FirstLastVowel.java
@@ -8,6 +8,20 @@ public class FirstLastVowel
 {
    public static String go( String a )
 	{
-		return "add code here";
+	   	String first = a.substring(0,1);
+	   	String last = a.substring(a.length()-1, a.length());
+	   	first = first.toUpperCase();
+	   	last = last.toUpperCase();
+	   
+	   	if ( first.contains("A") || first.contains("E") || first.contains("I") || 
+	   			first.contains("O") || first.contains("U") ) {
+	   		return "yes";
+	   	}
+	   	else if ( last.contains("A") || last.contains("E") || last.contains("I") || 
+	   			last.contains("O") || last.contains("U") ) {
+	   		return "yes";
+	   	}
+		
+	  return "no";
 	}
 }
