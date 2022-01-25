a37bea2df03bd6b091ce4968d0c876d576600296
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-25 08:10:16 -0800

    StringEquality Update

--- Unit05/src/StringEquality.java
+++ Unit05/src/StringEquality.java
@@ -1,9 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Emma Cao
+//Date - 1/24/22
+//Class - P. 1
 //Lab  -
 
 import static java.lang.System.*;
@@ -14,23 +14,33 @@ public class StringEquality
 
 	public StringEquality()
 	{
+		setWords("x", "y");
 	}
 
 	public StringEquality(String one, String two)
 	{
+		setWords(one, two);
 	}
 
 	public void setWords(String one, String two)
 	{
+		wordOne = one;
+		wordTwo = two;
 	}
 
 	public boolean checkEquality( )
 	{
+		if (wordOne.equals(wordTwo)) {
+			return true;
+		}
 		return false;
 	}
 
 	public String toString()
 	{
-		return wordOne + " does not have the same letters as " + wordTwo + "\n";
+		if (checkEquality() == false) {
+			return wordOne + " does not have the same letters as " + wordTwo + "\n";
+		}
+	return wordOne + " has the same letters as " + wordTwo + "\n";
 	}
 }
--- Unit05/src/StringEqualityRunner.java
+++ Unit05/src/StringEqualityRunner.java
@@ -1,9 +1,9 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
+//Name - Emma Cao
+//Date - 1/24/22
+//Class - P. 1
 //Lab  - 
 
 import static java.lang.System.*;
@@ -12,7 +12,39 @@ public class StringEqualityRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		StringEquality test = new StringEquality();
+		
+		test.setWords("hello", "goodbye");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("one", "two");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("three", "four");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("TCEA", "UIL");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("State", "Champions");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("ABC", "ABC");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("ABC", "CBA");
+		test.checkEquality();
+		System.out.print(test);
+		
+		test.setWords("Same", "Same");
+		test.checkEquality();
+		System.out.print(test);
 		
 	}
 }
