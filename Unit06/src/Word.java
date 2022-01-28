ead455db9f352fb58b6a2a78c3ce5587580b7c68
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-28 08:12:33 -0800

    Word Update

--- Unit06/src/Word.java
+++ Unit06/src/Word.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name Emma Cao 
 
 import static java.lang.System.*;
 
@@ -10,34 +10,42 @@ public class Word
 
 	public Word()
 	{
+		setString("");
 	}
 
 	public Word(String s)
 	{
+		setString(s);
 	}
 
 	public void setString(String s)
 	{
+		word = s;
 	}
 
 	public char getFirstChar()
 	{
-		return 0;
+		return word.charAt(0);
 	}
 
 	public char getLastChar()
 	{
-		return 0;
+		return word.charAt(word.length()-1);
 	}
 
 	public String getBackWards()
 	{
 		String back="";
+		for (int i = 0; i < word.length(); i++) {
+			back = back + word.charAt(word.length()-1-i);
+		}
 		return back;
 	}
 
  	public String toString()
  	{
- 		return "";
+ 		return String.valueOf(getFirstChar()) + "\n" + String.valueOf(getLastChar()) + "\n" +
+ 				getBackWards() + "\n" + word + "\n";
+ 		
 	}
 }
--- Unit06/src/WordRunner.java
+++ Unit06/src/WordRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name
+//Name Emma Cao 
 
 import static java.lang.System.*;
 
@@ -8,6 +8,22 @@ public class WordRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		Word test = new Word();
+		
+		test.setString("Hello");
+		System.out.println(test);
+		
+		test.setString("World");
+		System.out.println(test);
+		
+		test.setString("JukeBox");
+		System.out.println(test);
+		
+		test.setString("TCEA");
+		System.out.println(test);
+		
+		test.setString("UIL");
+		System.out.println(test);
 	}
+	
 }
