82b8a76473dc2a641d017954db85cdff3e68ebd4
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-28 09:09:56 -0800

    Perfect Update

--- Unit06/src/Perfect.java
+++ Unit06/src/Perfect.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Emma Cao
 
 import static java.lang.System.*;
 
@@ -8,15 +8,39 @@ public class Perfect
 {
    private int number;
 
-	//add constructors
-
-	//add a set method
-
-	public boolean isPerfect()
-	{
-		return false;
+   public Perfect() {
+		setNum(0);
 	}
+   
+   public Perfect(int num) {
+	   setNum(num);
+   }
+   
+   public void setNum(int num) {
+	   number = num;
+   }
 
-	//add a toString	
+   public boolean isPerfect()
+   {
+	   int numberCheck = 0;
+	   for (int i = 1; i < number; i++) {
+		   if (number % i == 0) {
+			   numberCheck = numberCheck + i;
+		   }
+	   }
+	   
+	   if (numberCheck == number) {
+		   return true;
+	   }
+	   return false;
+   }
+	
+   public String toString() {
+	   if (isPerfect() == true) {
+		   return String.valueOf(number) + " is perfect.";
+	   }
+	   return String.valueOf(number) + " is not perfect.";
+   }
+   
 	
 }
--- Unit06/src/PerfectRunner.java
+++ Unit06/src/PerfectRunner.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Emma Cao
 
 import static java.lang.System.*; 
 
@@ -8,7 +8,37 @@ public class PerfectRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		Perfect test = new Perfect();
+		
+		test.setNum(496);
+		System.out.println(test);
+		
+		test.setNum(45);
+		System.out.println(test);
+		
+		test.setNum(6);
+		System.out.println(test);
+		
+		test.setNum(14);
+		System.out.println(test);
+		
+		test.setNum(8128);
+		System.out.println(test);
+		
+		test.setNum(1245);
+		System.out.println(test);
+		
+		test.setNum(33);
+		System.out.println(test);
+		
+		test.setNum(28);
+		System.out.println(test);
+		
+		test.setNum(27);
+		System.out.println(test);
+		
+		test.setNum(33550336);
+		System.out.println(test);
 																
 	}
 }
