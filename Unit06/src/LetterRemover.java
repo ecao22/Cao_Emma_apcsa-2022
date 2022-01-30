04a49c434bd3fa26a1dd029baf0777c982b0a5d0
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-01-30 00:02:28 -0800

    LetterRemover Update2

--- Unit06/src/LetterRemover.java
+++ Unit06/src/LetterRemover.java
@@ -29,28 +29,15 @@ public class LetterRemover
 	{
 		String cleaned=sentence;
 		
-		/* if (cleaned.charAt(0) == lookFor) {
-			cleaned = cleaned.substring(1, cleaned.length());
-		}
-		
-		/* for (int i = 0; i < cleaned.length(); i++) {
-			if (cleaned.charAt(i) == lookFor) {
-				cleaned = cleaned.substring(0, i) + cleaned.substring(i+1);
-				// cleaned = cleaned.substring(i+1);
-			}
-		else {
-				cleaned = cleaned.substring(0);
-			}
-		} */
-		
-		int loc = cleaned.indexOf(lookFor);
+	int loc = cleaned.indexOf(lookFor);
 		
 		while (loc > -1) {
 			cleaned = cleaned.substring(0,loc) + cleaned.substring(loc+1);
 			loc = cleaned.indexOf(lookFor);
-		}
+		} 
 		
-		return cleaned;
+		return cleaned; 	
+	
 	}
 
 	public String toString()
