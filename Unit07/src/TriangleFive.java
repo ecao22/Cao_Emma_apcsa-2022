70ffe262429f79f9b0ae37ed0abd03cea83793e0
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-01 22:23:35 -0800

    TriangleFive Update

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,60 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter(' ');
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
 		String output="";
+		
+		
+		int toPrint = amount;
+		int columnNum = amount;
+		
+		for (int r = 0; r < amount; r++) {
+		
+			for (int c = 0; c < columnNum; c++) {
+
+				for (int s = 0; s < toPrint; s++) {
+					if (letter+amount-toPrint < 91) {
+						output += (char) (letter+amount-toPrint);
+					}
+					else {
+						output += (char) (letter+amount-toPrint-26);
+					}
+				}
+				output+=" ";
+				toPrint--;
+			}
+			output += "\n";
+			toPrint = amount;
+			columnNum--;
+			
+		}
+	
+		
+		
+		
+		
+		
 		return output;
 	}
 }
