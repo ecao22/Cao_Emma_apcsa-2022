a4343b84f82309e43135db186df47ba3184006c7
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-04 09:15:48 -0800

    OddToEven Update

--- Unit08/src/RayOddToEven.java
+++ Unit08/src/RayOddToEven.java
@@ -5,8 +5,34 @@
 
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int oddNumLoc = -1;
+		int evenNumLoc = -1;
+		
+		for (int i = 0; i < ray.length; i++) {
+			if (ray[i] % 2 == 1){
+				oddNumLoc = i;
+				break;
+			}
+		}
+		
+		if (oddNumLoc > -1) {
+			for (int j = oddNumLoc; j < ray.length; j++) {
+				if (ray[j] % 2 == 0) {
+					evenNumLoc = j;
+					break;
+				}
+			}
+		}
+		
+		if (evenNumLoc < 0) {
+			return -1;
+		}
+		else {
+			return evenNumLoc - oddNumLoc;
+
+		}
+		
 	}
 }
