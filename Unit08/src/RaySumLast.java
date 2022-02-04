f653c42d872cbae070c2aac8c14016a4c2c7e3a1
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-04 08:51:43 -0800

    RaySumLast Update

---
+++ Unit08/src/RaySumLast.java
@@ -0,0 +1,25 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class RaySumLast
+{
+	public static int go(int[] ray)
+	{
+		int last = ray[ray.length-1];
+		int sum = 0;
+		
+		for (int i = 0; i < ray.length-1; i++) {
+			if (ray[i] > last) {
+				sum = sum + ray[i];
+			}
+		}
+		
+		if (sum == 0) {
+			sum = -1;
+		}
+		
+		return sum;
+	}
+}
