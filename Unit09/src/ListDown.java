b22c5d6d5d641706f76e50a2d37918081de949d6
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-17 08:52:45 -0800

    ListDown Update

--- Unit09/src/ListDown.java
+++ Unit09/src/ListDown.java
@@ -12,6 +12,16 @@ public class ListDown
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(List<Integer> numArray)
 	{
+		
+		for (int i = 1; i < numArray.size(); i++) {
+			if (numArray.get(i) >= numArray.get(i-1)) {
+				return false;
+			}
+			else {
+				return true;
+			}
+		}
 		return true;
-	}	
+	}
+	
 }
