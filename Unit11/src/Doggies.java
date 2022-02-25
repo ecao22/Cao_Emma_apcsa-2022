8ca6ca026fc00472e2109530ba6ecdca7824e7cd
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-25 09:04:15 -0800

    DogUpdate

--- Unit11/src/Doggies.java
+++ Unit11/src/Doggies.java
@@ -4,6 +4,7 @@
 
 import java.util.Arrays;
 
+
 public class Doggies
 {
 	private Dog[] pups;
@@ -11,23 +12,40 @@ public class Doggies
 	public Doggies(int size)
 	{
 		//point pups at a new arry of Dog
+		pups = new Dog[size];
 		
 	}
 	
 	public void set(int spot, int age, String name)
 	{
 		//put a new Dog in the array at spot 
-		//make sure spot is in bounds		
+		//make sure spot is in bounds	
+		pups[spot] = new Dog(age, name);
 	}
 
 	public String getNameOfOldest()
 	{
-		return null;
+		int oldest = 0;
+		for (int i = 1; i < pups.length; i++) {
+			if (pups[i].getAge() > oldest) {
+				oldest = i;
+			}
+		}
+		
+		return pups[oldest].getName();
+		
 	}
 
 	public String getNameOfYoungest()
 	{
-		return null;
+		int youngest = 0;
+		for (int i = 1; i < pups.length; i++) {
+			if (pups[i].getAge() < youngest) {
+				youngest = i;
+			}
+		}
+		
+		return pups[youngest].getName();
 	}
 
 	public String toString()
