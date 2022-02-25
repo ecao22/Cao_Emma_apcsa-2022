b2fec29f8852ef9fbe5bd757c0f5cae1f49be27d
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-02-25 09:24:39 -0800

    TriangleWord Update

--- Unit11/src/TriangleWord.java
+++ Unit11/src/TriangleWord.java
@@ -11,5 +11,11 @@ public class TriangleWord
 		
 	public static void printTriangle(String word)
 	{
+		for (int i = 0; i < word.length()+1; i++) {
+			for (int j = 0; j < word.substring(0,i).length(); j++) {
+				System.out.print(word.substring(0,i));
+			}
+			System.out.print("\n");
+		}
 	}
 }
