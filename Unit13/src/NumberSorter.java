7425f9a0b2b3105978323b140b5cf1b584a7996a
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-03-17 09:11:42 -0700

    NumberSorter Update

--- Unit13/src/NumberSorter.java
+++ Unit13/src/NumberSorter.java
@@ -12,15 +12,36 @@ public class NumberSorter
 {
 	//instance variables and other methods not shown
 
+
 	private static int getNumDigits(int number)
 	{
-		int count = 0;
+		int count = number%10;
 		return count;
 	}
 
 	public static int[] getSortedDigitArray(int number)
 	{
-		int[] sorted = null;
+		int length = (int) (Math.log10(number) + 1);
+		int[] sorted = new int[length];
+		for (int i = 0; i < length; i++) {
+			sorted[i] = getNumDigits((int)(number/(Math.pow(10, i))));
+		}
+		
+
+		for (int j = 0; j < length-1; j++) {
+			int min = j;
+			for (int k = j+1; k < length; k++) {
+				if (sorted[k] < sorted[min]) {
+					min = k;
+				}
+			}
+			if (min != j) {
+				int temp = sorted[min];
+				sorted[min] = sorted[j];
+				sorted[j] = temp;
+			}
+		}
+		
 		return sorted;
 	}
 }
