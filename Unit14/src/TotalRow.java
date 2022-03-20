6e81517c28f2fb25f8c95056c26e3182cf61df20
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-03-19 21:23:44 -0700

    TotalRow Update

--- Unit14/src/TotalRow.java
+++ Unit14/src/TotalRow.java
@@ -8,7 +8,15 @@ public class TotalRow
 {
     public static List<Integer> getRowTotals( int[][] m )
     {
-    	//add code here
-		return null;
+    	ArrayList<Integer> totals = new ArrayList<Integer>();
+    	int total = 0;
+    	for (int i = 0; i < m.length; i++) {
+    		for (int j = 0; j < m[i].length; j++) {
+    			total += m[i][j];
+    		}
+    		totals.add(total);
+    		total = 0;
+    	}
+		return totals;
     }
 }
