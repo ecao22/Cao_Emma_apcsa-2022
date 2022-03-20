09641a6e4810519b6a9cbf20f0cc39a57a9398a0
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-03-19 21:14:52 -0700

    RecursionOne Update

--- Unit14/src/RecursionFunOne.java
+++ Unit14/src/RecursionFunOne.java
@@ -8,8 +8,14 @@ public class RecursionFunOne
 {
 	public static int countOddDigits(int num)
 	{
-
-
+		if (num > 0) {
+			if (num % 2 == 0) {
+				return 1 + countOddDigits(num/10);
+			}
+			return 0 + countOddDigits(num/10);
+		}
+		
 		return 0;
+
 	}
 }
