8926b95bcb0e178ac04d6db6a7a05698c3cf2354
ecao22 <97621289+ecao22@users.noreply.github.com>
2022-03-19 21:08:00 -0700

    MatrixCount1 Update

--- Unit14/src/MatrixCount1Runner.java
+++ Unit14/src/MatrixCount1Runner.java
@@ -6,7 +6,14 @@ public class MatrixCount1Runner
 {
 	public static void main( String args[] ) throws Exception
 	{
-		//add code
+		System.out.println("Matrix values");
+
+		
+		System.out.println("The 7 count is :: " + MatrixCount1.count(7));
+		System.out.println("The 8 count is :: " + MatrixCount1.count(8));
+		System.out.println("The 6 count is :: " + MatrixCount1.count(6));
+		System.out.println("The 21 count is :: " + MatrixCount1.count(21));
+		
 	}
 }
 
